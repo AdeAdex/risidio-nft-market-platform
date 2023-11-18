e40bb0a61d2af9390f14e5bf290fe06448443789
Adeolu Amole <adeoluamole@gmail.com>
2023-11-18 07:51:57 +0100

    Formatting the code at card component to make it more beautiful

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -7,6 +7,8 @@ import {
   increaseQuantity,
 } from "../redux/wishlistSlice";
 import { useSelector } from "react-redux";
+import CardImage from "./cardComponent/CardImage";
+import CardDetails from "./cardComponent/CardDetails";
 
 const Cards = ({ collection }) => {
   const dispatch = useDispatch();
@@ -68,24 +70,8 @@ const Cards = ({ collection }) => {
               key={index}
               state={eachCollection}
             >
-              <img
-                src={eachCollection.photo}
-                alt={eachCollection.title}
-                className="relative mx-0 -mt-6 h-40 w-full overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40 bg-gradient-to-r from-blue-500 to-blue-600"
-              />
-
-              <div className="p-6">
-                <h5 className="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
-                  {eachCollection.title}
-                </h5>
-                <p className="text-sm">{eachCollection.categories}</p>
-                <p
-                  className="block font-sans text-base font-light leading-relaxed text-inherit antialiased"
-                  style={{ height: "50px" }}
-                >
-                  <small> {eachCollection.summaries.slice(0, 50)}...</small>
-                </p>
-              </div>
+              <CardImage eachCollection={eachCollection} />
+              <CardDetails eachCollection={eachCollection} />
             </Link>
             <div className="p-6 pt-0 mt-3 flex justify-between">
               {wishlist.some((item) => item.title === eachCollection.title) ? (
